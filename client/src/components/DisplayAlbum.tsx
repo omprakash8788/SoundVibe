afc1348ed6e351911234bbe0842df33faab434e5
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-12 22:58:23 +0530

    single page song ui

--- client/src/components/DisplayAlbum.tsx
+++ client/src/components/DisplayAlbum.tsx
@@ -1,18 +1,58 @@
-import { useParams } from "react-router-dom"
-import Navbar from "./Navbar"
-import { albumsData } from "../assets/assets";
+import { useParams } from "react-router-dom";
+import Navbar from "./Navbar";
+import { albumsData, assets, songsData } from "../assets/assets";
 
 const DisplayAlbum = () => {
-    const {id} = useParams();
-    const AlbumData = albumsData[id]
-    console.log(AlbumData)
+  const { id } = useParams();
+  const AlbumData = albumsData[id];
 
   return (
     <>
-    <Navbar/>
-
+      <Navbar />
+      <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-end">
+        <img className="w-48 rounded" src={AlbumData.image} alt="" />
+        <div className="flex flex-col">
+          <p>Playlist</p>
+          <h2 className="text-5xl font-bold mb-4 md:text-7xl">
+            {AlbumData.name}
+          </h2>
+          <h4>{AlbumData.desc}</h4>
+          <p className="mt-1 flex gap-1.5">
+            <img
+              className="inline-block w-5"
+              src={assets.spotify_logo}
+              alt=""
+            />
+            <b>Spotify</b>* 6,65, 655 likes* <b>50 songs,</b> about 2 hr 45 min
+          </p>
+        </div>
+      </div>
+      {/*  */}
+      <div className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]">
+        <p>
+          <b>#</b>
+        </p>
+        <p>Album</p>
+        <p className="hidden sm:block">Date Added</p>
+        <img className="m-auto w-4" src={assets.clock_icon} alt="icon" />
+      </div>
+      <hr />
+      {songsData.map((item, index) => (
+        <div
+          key={index}
+          className="grid grid-cols-3 sm:grid-cols-4 gap-2 items-center p-2 text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
+        >
+          <p className="text-white">
+            <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
+            <img className="inline w-10 mr-5" src={item.image} alt="img" />
+          </p>
+          <p className="text-sm">{AlbumData.name}</p>
+          <p className="text-sm hidden sm:block">2 days ago</p>
+          <p className="text-sm text-center">{item.duration}</p>
+        </div>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default DisplayAlbum
+export default DisplayAlbum;
