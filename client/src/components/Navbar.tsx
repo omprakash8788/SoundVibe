bf571d8096e955f76eddd848f221442ab1740605
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-13 00:07:22 +0530

    fixing bugs in back and next btn

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,16 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
           <img
+            onClick={() => navigate("/")}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer transition-all duration-200 hover:scale-105 hover:bg-gray-800"
             src={assets.arrow_left}
             alt=""
           />
           <img
+            onClick={() => navigate(+1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer transition-all duration-200 hover:scale-105 hover:bg-gray-800"
             src={assets.arrow_right}
             alt=""
