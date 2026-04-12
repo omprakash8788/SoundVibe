83a43a6dde124c5d1bd054e346312662ccf3944c
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-12 22:21:44 +0530

    single page song ui

--- client/src/components/AlbumItem.tsx
+++ client/src/components/AlbumItem.tsx
@@ -1,12 +1,18 @@
-const AlbumItem = ({image, name, desc, id}) => {
-  // id - add id 
-  return (
-    <div className="min-w-45 p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
-        <img className="rounded" src={image} alt="img" />
-        <p className="font-bold mt-2 mb-1">{name}</p>
-        <p className="text-slate-200 text-sm">{desc}</p>
-    </div>
-  )
-}
+import { useNavigate } from "react-router-dom";
 
-export default AlbumItem
+const AlbumItem = ({ image, name, desc, id }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      onClick={() => navigate(`/album/${id}`)}
+      className="min-w-45 p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]"
+    >
+      <img className="rounded" src={image} alt="img" />
+      <p className="font-bold mt-2 mb-1">{name}</p>
+      <p className="text-slate-200 text-sm">{desc}</p>
+    </div>
+  );
+};
+
+export default AlbumItem;
