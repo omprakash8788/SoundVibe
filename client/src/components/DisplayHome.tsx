fa499c98d96ffd827f44249e8da275effec1907f
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-12 22:09:11 +0530

    adding album component

--- client/src/components/DisplayHome.tsx
+++ client/src/components/DisplayHome.tsx
@@ -1,11 +1,27 @@
-import Navbar from "./Navbar"
+import { albumsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+import Navbar from "./Navbar";
 
 const DisplayHome = () => {
   return (
-    <div>
-        <Navbar/>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Features Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              image={item.image}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default DisplayHome
+export default DisplayHome;
