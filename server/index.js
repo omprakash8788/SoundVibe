e91c9faced4647cea9247f8914ae321f431f6b7c
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-16 23:10:29 +0530

    server setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log("Hello com vision")
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("API working"));
+app.listen(port, () => console.log(`Server started on ${port}`));
