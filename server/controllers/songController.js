4decf5f53bbe442ed572c5696fb50987f18c6139
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-18 00:00:38 +0530

    db connection

---
+++ server/controllers/songController.js
@@ -0,0 +1,10 @@
+
+const addSong=async(req, res)=>{
+
+}
+
+const listSong=async(req, res)=>{
+
+}
+
+export {addSong, listSong}
--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import songRouter from "./routes/songRoute.js";
+import connectDB from "./config/mongodb.js";
 
-const app = express();
+const app = express(); 
 const port = process.env.PORT || 5000;
+connectDB()
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/song", songRouter)
 
 app.get("/", (req, res) => res.send("API working"));
 app.listen(port, () => console.log(`Server started on ${port}`));
