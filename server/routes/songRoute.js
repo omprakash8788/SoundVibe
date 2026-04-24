4c9a16b1b8538c9d78fec279abbdca578837d9ef
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-24 22:18:22 +0530

    creating upload middleware

--- server/routes/songRoute.js
+++ server/routes/songRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { addSong, listSong } from "../controllers/songController.js";
+import upload from "../middlewares/multer.js";
 
 const songRouter = express.Router();
-songRouter.post("/add", addSong)
+songRouter.post("/add", upload.fields([{name:"image", maxCount:1}, {name:"audio", maxCount:1}]) ,addSong)
 songRouter.get("/list", listSong)
 
 export default songRouter;
