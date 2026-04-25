d655afa0b13d74470e84ae8f8735d4ab8b19f84a
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-26 01:27:45 +0530

    album

---
+++ server/controllers/albumController.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from "cloudinary";
+import albumModel from "../models/albumModel.js";
+
+const addAlbum = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const listAlbum = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const removeAlbum = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+export { addAlbum, listAlbum, removeAlbum };
