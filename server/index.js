08f4ab5e3797a49936f2a19d5df55ad26c65d414
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-26 23:42:10 +0530

    album controller setup

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import songRouter from "./routes/songRoute.js";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import albumRouter from "./routes/albumRoute.js";
 
 const app = express(); 
 const port = process.env.PORT || 5000;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/song", songRouter)
+app.use("/api/album",albumRouter) 
 
 app.get("/", (req, res) => res.send("API working"));
 app.listen(port, () => console.log(`Server started on ${port}`));
