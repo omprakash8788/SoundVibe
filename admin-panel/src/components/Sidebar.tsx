2dfe8b6c1e5d8c88fd453a90f70f7ba263aaa65f
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-27 23:05:46 +0530

    sidebar layout setup

--- admin-panel/src/components/Sidebar.tsx
+++ admin-panel/src/components/Sidebar.tsx
@@ -1,8 +1,39 @@
+import { assets } from "../assets/assets";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="bg-[#003a10] min-h-screen pl-[4vw]">Sidebar</div>
-  )
-}
+    <div className="bg-[#003a10] min-h-screen pl-[4vw]">
+      <img className="mt-5 w-24 hidden sm:block" src={assets.logo} alt="logo" />
+      <img
+        className="mt-5 w-12 mr-5 sm:hidden block"
+        src={assets.logo_small}
+        alt="logo"
+      />
+      <div className="flex flex-col gap-5 mt-10">
+        <NavLink to="/add-song" className="flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-16  drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium">
+          <img className="w-5" src={assets.add_song} alt="adsong" />
+          <p className="hidden sm:block">Add Song</p>
+        </NavLink>
 
-export default Sidebar
+         <NavLink to="/list-song" className="flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-16  drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium">
+          <img className="w-5" src={assets.song_icon} alt="adsong" />
+          <p className="hidden sm:block">List Song</p>
+        </NavLink>
+
+         <NavLink to="/add-album" className="flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-16  drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium">
+          <img className="w-5" src={assets.add_album} alt="adsong" />
+          <p className="hidden sm:block">Add Album</p>
+        </NavLink>
+
+         <NavLink to="/list-album" className="flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-16  drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium">
+          <img className="w-5" src={assets.album_icon} alt="adsong" />
+          <p className="hidden sm:block">List Album</p>
+        </NavLink>
+       
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
