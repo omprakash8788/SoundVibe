e298a7615b2aef7adf4ff75c4b3d11243443d692
omprakash8788 <omprakashkumar552@gmail.com>
2026-04-27 22:29:50 +0530

    router setup

--- admin-panel/src/App.tsx
+++ admin-panel/src/App.tsx
@@ -1,7 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import AddSong from "./pages/AddSong";
+import AddAlbum from "./pages/AddAlbum";
+import ListSong from "./pages/ListSong";
+import ListAlbum from "./pages/ListAlbum";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
-    <div className="bg-red-500">App</div>
-  )
-}
-export default App
+    <>
+      <div className="flex items-start min-h-screen">
+        <ToastContainer />
+        <Sidebar />
+        <div className="flex-1 h-screen overflow-y-scroll bg-[#f3fff7]">
+          <div className="pt-8 pl-5 sm:pt-12 sm:pl-12">
+            <Routes>
+              <Route path="/add-song" element={<AddSong />} />
+              <Route path="/add-album" element={<AddAlbum />} />
+              <Route path="/list-song" element={<ListSong />} />
+              <Route path="/list-album" element={<ListAlbum />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default App;
---
+++ admin-panel/src/components/Sidebar.tsx
@@ -0,0 +1,8 @@
+
+const Sidebar = () => {
+  return (
+    <div className="bg-[#003a10] min-h-screen pl-[4vw]">Sidebar</div>
+  )
+}
+
+export default Sidebar
