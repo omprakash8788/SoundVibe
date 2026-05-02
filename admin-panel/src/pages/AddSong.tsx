3681887cecb0b8f3b824ce7b0caa89e2ad4f69db
omprakash8788 <omprakashkumar552@gmail.com>
2026-05-02 23:59:54 +0530

    [feat]-implemented dropdown

--- admin-panel/src/pages/AddSong.tsx
+++ admin-panel/src/pages/AddSong.tsx
@@ -1,4 +1,4 @@
-import {useState } from "react";
+import { useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { toast } from "react-toastify";
 import axios from "axios";
@@ -14,39 +14,54 @@ const AddSong = () => {
   const [loading, setLoading] = useState(false);
   const [albumData, setAlbumData] = useState([]);
 
-
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     setLoading(true);
     try {
-        const formData = new FormData();
-        formData.append("name",name);
-        formData.append("desc", desc);
-        formData.append("image", image);
-        formData.append("audio", song);
-        formData.append("album", album)
-        const response = await axios.post(`${url}/api/song/add`, formData)
-        // console.log(response)
-        if(response.data.success){
-            toast.success("Song Added");
-            setName("");
-            setAlbum("None");
-            setDesc("");
-            setImage(false);
-            setSong(false)
-        }else{
-            toast.error("Something went wrong")
-        }
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("desc", desc);
+      formData.append("image", image);
+      formData.append("audio", song);
+      formData.append("album", album);
+      const response = await axios.post(`${url}/api/song/add`, formData);
+      // console.log(response)
+      if (response.data.success) {
+        toast.success("Song Added");
+        setName("");
+        setAlbum("None");
+        setDesc("");
+        setImage(false);
+        setSong(false);
+      } else {
+        toast.error("Something went wrong");
+      }
     } catch (error) {
-        toast.error("Error occured")
-        console.log(error)
+      toast.error("Error occured");
+      console.log(error);
     }
-    setLoading(false)
-
+    setLoading(false);
   };
 
+  useEffect(() => {
+    const loadAlbumData = async () => {
+      try {
+        const response = await axios.get(`${url}/api/album/list`);
+        if (response.data.success) {
+          setAlbumData(response.data.albums);
+        } else {
+          toast.error("unable to load albums data");
+        }
+      } catch (error) {
+        toast.error("Error occured");
+        console.log(error);
+      }
+    };
+    loadAlbumData();
+  }, []);
+
   return loading ? (
-   <Spiner/>
+    <Spiner />
   ) : (
     <form
       onSubmit={onSubmitHandler}
@@ -122,6 +137,13 @@ const AddSong = () => {
           className="bg-transparent outline-green-600 border-2 border-gray-400 p-2 w-37.5"
         >
           <option value="none">None</option>
+          {
+            albumData.map((item, index)=>(
+              <option key={index} value={item.name}>
+                {item.name}
+              </option>
+            ))
+          }
         </select>
       </div>
 
