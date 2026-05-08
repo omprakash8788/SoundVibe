da11d32ec428945370f3b68d1ef36d0d6fe9eee8
omprakash8788 <omprakashkumar552@gmail.com>
2026-05-08 20:49:16 +0530

    adding rate limiter

--- server/middlewares/rateLimiter.js
+++ server/middlewares/rateLimiter.js
@@ -4,24 +4,18 @@ const rateLimiter = (req, res, next) => {
     const ip = req.ip;
     const limit = 100;
     const windowMs = 60 * 1000; 
-
     const now = Date.now();
-    
     if (!requestCounts.has(ip)) {
         requestCounts.set(ip, { count: 1, startTime: now });
         return next();
     }
-
     const userData = requestCounts.get(ip);
-
     if (now - userData.startTime > windowMs) {
         userData.count = 1;
         userData.startTime = now;
         return next();
     }
-
     userData.count++;
-
     if (userData.count > limit) {
         return res.status(429).json({ 
             message: "Too many requests, please try again later." 
@@ -29,5 +23,4 @@ const rateLimiter = (req, res, next) => {
     }
     next();
 };
-
 export default rateLimiter
